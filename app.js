947eee3500cf29c30b4d96a8099c9d1f41f13bd1
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2023-01-17 23:22:10 +0000

    [Add]practice

--- app.js
+++ app.js
@@ -29,11 +29,12 @@
 
 // alert('2をかけ続けて'　+ max + 'を超えるのに必要だった回数は' + count + '回です');
 
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for (i = 1; i < 11; i++){
-    num = num + i;
-}
+// for (i = 1; i < 11; i++){
+//     num = num + i;
+// }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+// alert('1から10まで足し算した結果は' + num + 'です');
+

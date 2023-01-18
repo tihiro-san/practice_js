f44baf13e2a7aaec30ff08e8fd8bc22ac68cab25
tihiro-san <ec2-user@ip-172-31-44-178.ap-northeast-1.compute.internal>
2023-01-18 01:37:50 +0000

    [Update]app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // let orange = 100;
 
-// let apple = 100; 
+// let apple = 100;
 
 // if (orange < apple){
 //     alert('みかんの値段がりんごより安い');
@@ -38,3 +38,82 @@
 
 // alert('1から10まで足し算した結果は' + num + 'です');
 
+// let alertString;
+
+// alertString = addString("Webcamp");
+
+// alert(alertString);
+
+// function addString(strA){
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+// let promptStr = prompt('何か好きな文字を入力してください。');
+
+// alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んで下さい。');
+
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んで下さい。');
+}
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' +js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジして")
+}
+
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random()*3 );
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  }else if(js_hand_num == 1){
+    hand_name ="チョキ";
+  }else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
+}
+
+function winLose(user,js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    }else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    }else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr == "負け";
+    } else if(js == "チョキ"){
+      winLoseStr == "あいこ";
+    } else if(js== "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr == "勝ち";
+    }else if(js == "チョキ"){
+      winLoseStr == "負け";
+    }else if(js == "パー"){
+      winLoseStr == "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
